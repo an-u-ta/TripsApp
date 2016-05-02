d6b972107515da1622f23e0d58216b8e7f2997d3
annya_kiz <annya_kiz@hotmail.com>
2016-05-01 23:48:13 -0400

    SecondCommit

---
+++ Core1/Core1/src/Core1/Controllers/API/TripsController.cs
@@ -0,0 +1,29 @@
+﻿using Core1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Core1.ViewModels;
+using AutoMapper;
+
+namespace Core1.Controllers.API
+{
+    public class TripsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Json()
+        {
+            TripsRepository db = new TripsRepository();
+            //JsonResult.Trip = new Trip()
+           // {
+            //    Name = "First Trip",
+            //    DateCreated = DateTime.Now
+          //  };
+            var trips = db.GetAllTrips();
+            var results = Mapper.Map<IEnumerable<TripViewModel>>(trips);
+            return Json(); // past trips to the JsonResult
+        }
+
+    }
+}
